4da1e458957f4b71233363e995b98fb6f4eded80
lDrak <Drakula3D@mail.ru>
2024-03-11 13:41:41 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkCpp13/Helpers.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <cmath>
+
+int QuadratSum(int num1, int num2) {
+	return std::pow(num1 + num2, 2);
+}
---
+++ HomeWorkCpp13/HomeWorkCpp13.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+    int Number1 = 0;
+    int Number2 = 0;
+    std::cout << "Enter Number 1: ";
+    std::cin >> Number1;
+    
+    std::cout << "Enter Number 2: ";
+    std::cin >> Number2;
+    std::cout << QuadratSum(Number1, Number2);
+}
+
